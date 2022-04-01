93f153ae1505c78b2d7ceb07f5d21b8554ab7d08
nguyenthinga-0206 <nga3421@gmail.com>
2022-04-01 09:01:17 +0700

    FE

--- spring-boot-refresh-token-jwt/src/main/java/com/bezkoder/spring/security/jwt/constan/Constan.java
+++
@@ -1,6 +0,0 @@
-package com.bezkoder.spring.security.jwt.constan;
-
-public final class Constan {
-    public static final String START_DATE = "1000-01-01T00:00:00";
-    public static final String END_DATE = "9999-01-01T00:00:00";
-}
--- spring-boot-refresh-token-jwt/src/main/java/com/bezkoder/spring/security/jwt/utils/DateTimeUtils.java
+++
@@ -1,9 +0,0 @@
-package com.bezkoder.spring.security.jwt.utils;
-
-import java.time.LocalDateTime;
-
-public class DateTimeUtils {
-    public static LocalDateTime convertLocalDateTime(String dateTime) {
-        return LocalDateTime.parse(dateTime);
-    }
-}
